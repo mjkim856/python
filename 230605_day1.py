3fea73b09079e8a6bf16e53ba458b8ee3676e144
mjkim856 <mjkim856@gmail.com>
2023-06-07 11:03:14 +0900

    learn : 자료형, 구구단, 문자열, string, indexing 인덱싱 연습

--- 230605_day1.py
+++ 230605_day1.py
@@ -1,3 +1,4 @@
+# 자료형, 구구단, 문자열, string, indexing 인덱싱 연습
 # 파이썬 지원 자료형
 '''
 int : 정수
@@ -88,7 +89,7 @@ print(type(b_str1), len(b_str1))
 
 
 # 문자열 연산
-str_o1 = "Python"
+str_o1 = "Python "
 str_o2 = "Apple"
 str_o3 = "How are you doing?"
 str_o4 = "Seoul Deajeon Busan Jinju"
@@ -97,6 +98,7 @@ print("\n문자열 연산")
 print(3 * str_o1)
 print(str_o1 + str_o2)
 print(dir(str_o1))
+print(len(str_o1))
 print('y' in str_o1)
 print('n' in str_o1)
 print('P' not in str_o2)
@@ -105,3 +107,32 @@ print('P' not in str_o2)
 # awk '{print $3}' lastb.txt | grep ^[0-9] | sort -n | uniq -c
 # awk '{print $3}' lastb.txt | grep ^[0-9] | sort -n | uniq -c | awk '$1 >=20{print $0}'
 # 이걸 사용해서 방화벽이랑 뭐... 브루트포스 공격 방어 가능하다. 
+
+print("\n문자열 냅다 곱하기")
+print("="*50)
+
+# 냅다 문자열 인덱싱
+str_index="Plz Index Str"
+#          0123456789012
+print(str_index[4])
+print(str_index[0:5])
+print(str_index[0] + str_index[4] + str_index[10])
+print(str_index[-1])
+print(str_index[-2])
+print("str_index[1:-1:2]", str_index[1:-1:2]) # 1에서 -1까지 가는데 2개 단위로 출력
+print("str_index[::2]", str_index[::2])
+print()
+
+for i in str_index:
+    print(ord(i))   # 아스키코드로 변환하기
+
+print()
+
+for i in range(2, 10):
+    print('%d단 시작!' % i)
+    for j in range(1, 10):
+        print('%d * %d = %d' % (i, j, i*j))
+    print('%d단 끝! \n' % i)
+
+# shell에서 반복문 사용하기
+# for i in $(echo $str_index)
