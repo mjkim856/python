7892bb03c3ece7c73ea1cc9c7beaa46f0592ed39
mjkim856 <mjkim856@gmail.com>
2023-06-07 14:27:35 +0900

    learn : list 할당, 복사

--- 230607_day2.py
+++ 230607_day2.py
@@ -83,15 +83,46 @@ print("\nappend, sort, reverse, insert, remove, pop, extend")
 l1.append(0)
 print(l1.append(0)) # 특이하게 이 경우는 None이 뜬단 말이지?
 print(l1)
-l1.sort();      print(l1)
-l1.reverse();   print(l1)
-l1.insert(0,1); print(l1)
-l1.remove(1);   print(l1)
-l1.pop();       print(l1)
+l1.sort();                   print(l1)
+l1.reverse();                print(l1)
+l1.insert(0,1);              print(l1)
+l1.remove(1);                print(l1)
+l1.pop();                    print(l1)
 l1.extend(['only', 'list']); print(l1)
+del(l1[-1]);                 print(l1) # 인덱스 번호 확실히 알아야 함.
 
 
+# 리스트 할당. 굉장히 특이하다. 자바도 이랬었나?
+print("\n리스트 할당")
+x = [1,2,3,4,5]
+y = x 
+y[2] = 0
+print(x)        # [1, 2, 0, 4, 5]
+print(y)        # [1, 2, 0, 4, 5]
+print(id(x))    # 139695902380544
+print(id(y))    # 139695902380544
+x[3] = 123
+print(x)        # [1, 2, 0, 123, 5]
+print(y)        # [1, 2, 0, 123, 5]
 
+# 리스트 복사
+print("\n리스트 복사")
+a = [5,6,7,8,9]
+b = a.copy()
+b[2] = 2
+print(a)        # [5, 6, 7, 8, 9]
+print(b)        # [5, 6, 2, 8, 9]
+print(id(a))    # 139695902333184
+print(id(b))    # 139695902380608
 
-
+# 중첩 리스트 복사
+print("\n중첩 리스트 복사")
+import copy
+c = [[1,2], [3,4,5]]
+d = copy.deepcopy(c)
+d[0][0] = 0
+print(c)        # [[1, 2], [3, 4, 5]]
+print(d)        # [[0, 2], [3, 4, 5]]
+print(id(c))    # 139695902030144
+print(id(d))    # 139695903207232
 
