92173563d0918f29eddc83780de73fb83f3265c2
mjkim856 <mjkim856@gmail.com>
2023-06-08 11:59:58 +0900

    learn : 집합 예제

--- dictionary.py
+++ dictionary.py
@@ -195,3 +195,137 @@ def valuetokey(b):
 value='보'
 key=valuetokey(value)
 print("\n{}의 key 값은 {} 입니다.".format(value,key))
+
+# -------------------------[ 집합 ]------------------------
+# 집합(Sets) 특징   
+# 딕셔너리, 리스트, 튜플에서 각 각 특징을 가짐   
+# 선형대수학, 데이터 분석에서 numpy, scipy (싸이파이) 를 이용해서 사용
+# 집합(Sets) 자료형(순서X, 중복X)  집합에서는 어떤 원소가 존재하는 지가 더 중요
+
+# 선언
+# () 튜플  [] 리스트  {} 딕셔너리, 집합
+a = set()   
+b = set([1, 2, 3, 4])
+c = set([1, 4, 5, 6])
+d = set([1, 2, 'Pen', 'Cap', 'Plate'])  # 서로 다른 자료형 선언 가능
+e = {'foo', 'bar', 'baz', 'foo', 'qux'}  # 딕셔너리와 다른부분은 키가 없고 리스트 처럼  { } 안에서 나열
+f = {42, 'foo', (1, 2, 3), 3.14159}
+
+print("\n--------------------------------- 집합 시작 ----------------------------------")
+print('a - ', type(a), a)
+# b = set([1, 2, 3, 4, 4, 4, 4])    # 중복을 허용 하지 않는다.
+print('b - ', type(b), b)
+# print('b - ', type(b), b, 2 in a)    # in 연산자도 사용할 수 있다.  
+# print('b - ', type(b), b, 2 in b)    # 튜플이나 , 리스트, 딕셔너리에서 사용할 수 있는다 다할 수 있다. 
+print('c - ', type(c), c)
+print('d - ', type(d), d)
+print('e - ', type(e), e)
+print('f - ', type(f), f)
+
+# 튜플 변환
+t = tuple(b)
+print("\n출력 튜플로 변환")
+print('t - ', type(t), t)
+print('t - ', t[0], t[1:3])  # 튜플이 되었다는 것은 슬라이싱 할 수 있다는것
+
+# 리스트 변환
+l = list(c)
+l2 = list(e)
+print("\n출력 리스트로 변환")
+print('l - ', type(l), l)
+print('l - ', l[0], l[1:3])
+print('l2 - ', type(l2), l2)
+
+# 길이
+print("\n출력 길이")
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+print(len(e))
+print(len(f))
+
+# 집합 자료형 활용  (교집합(&), 합집합( |). 차집합(-) )
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6, 7, 8, 9])
+print("\n출력 집합 자료형 활용")
+print('s1 & s2 : ', s1 & s2)
+print('s1 & s2 : ', s1.intersection(s2))  
+
+print('s1 | s2 : ', s1 | s2)
+print('s1 | s2 : ', s1.union(s2))
+
+print('s1 - s2 : ', s1 - s2)
+print('s1 -  s2 : ', s1.difference(s2))
+
+# 중복 원소 확인 (교집합인지 확인)
+print("\n출력 중복 원소 확인")
+print('l - ', s1.isdisjoint(s2))   # false 가 나올때 중복원소가 존재함을 의미 
+
+# 부분 집합 확인 
+#s1 = set([1, 2, 3, 4, 5, 6])
+#s2 = set([4, 5, 6])
+print("\n출력 부분 집합 확인")
+print('subset ', s1.issubset(s2))   # False   부분집합 체크
+print('subset', s2.issubset(s1))   # True
+print('superset', s1.issuperset(s2))  
+
+# 추가 & 제거
+# set 에서는  add, remove , discard, clear 메소드 하용
+print("\n출력 add remove discard clear")
+s1 = set([1, 2, 3, 4])  
+s1.add(5)
+print('s1 - ', s1)
+
+s1.remove(2)
+print('s1 - ', s1)
+# s1.remove(7)   # 없는 원소를 삭제 하려면 keyerror 메시지 발생
+
+s1.discard(3)
+print('s1 - ', s1)
+
+#s1.discard(7)    #[중요] 없는 원소를 삭제 하려 해도 에러가 발생하지 않는다.  결국 remove 보다는 discard 사용
+
+# 모두 제거
+s1.clear()
+print('s1 - ', s1)
+
+a = [1, 2, 3] #리스트도 clear로 삭제 가능
+a.clear()
+print(a)
+
+# 집합 실습
+# a = [1,2,3,4] 로 set s1을 생성하시오.
+print("\n집합 실습")
+s1=set(a)
+
+# b = "aabbccddeeff"로 set s2를 생성하시오.
+s2=set(b)
+
+# s1 에 a,b,c 를 추가하시오.
+s1.add('a')
+s1.add('b')
+s1.add('c')
+# 혹은
+s1.update(['a','b','c'])
+
+# s2 에 1,2를 추가하시오.
+s1.update([1,2])
+
+# s1과 s2의 교집합을 구하시오.(2가지 방법 모두 )
+print("교집합:",s1 & s2)
+print("교집합:",s1.intersection(s2))
+
+# s1과 s2의 합집합을 구하시오.(2가지 방법 모두)
+s1 | s2
+print("합집합:",s1.union(s2))
+
+# s1과 s2의 차집합을 구하시오.(기호)
+print("차집합:",s1-s2)
+
+# s2와 s1의 차집합을 구하시오.(함수)
+print("차집합:",s2.difference(s1))
+
+# s2에서 1을 빼보시오.
+s2.remove(1)
+print("remove:",s2)
